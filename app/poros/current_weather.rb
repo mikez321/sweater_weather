85235bfe88a3f62cfd38f3cad2a960cb663f2675
Mike Hernandez <hernandezmichael321@gmail.com>
2020-06-06 16:16:28 -0600

    Move all methods into initialize for serializer

--- app/poros/current_weather.rb
+++ app/poros/current_weather.rb
@@ -1,6 +1,7 @@
 class CurrentWeather
-  attr_reader :time,
-              :name,
+  attr_reader :date,
+              :time,
+              # :name,
               :conditions,
               :icon,
               :current_temp,
@@ -8,17 +9,26 @@ class CurrentWeather
               :high,
               :low,
               :humidity
+              :visibility
 
   def initialize(current_info)
-    # @current = current_info
-    @time = Time.at(current_info[:dt]).strftime("%l:%m %p, %B %-d")
-    @name = current_info[:name]
-    @conditions = current_info[:weather].first[:description].titleize
-    @icon = current_info[:weather].first[:icon]
-    @current_temp = current_info[:main][:temp].to_i
-    @feels_like = current_info[:main][:feels_like].to_i
-    @high = current_info[:main][:temp_max].to_i
-    @low = current_info[:main][:temp_min].to_i
-    @humidity = current_info[:main][:humidity].to_i
+    @date = Time.at(current_info[:current][:dt]).strftime("%m/%d/%Y")
+    @time = current_info[:current][:dt]
+    @conditions = current_info[:current][:weather].first[:description]
+    @icon = current_info[:current][:weather].first[:icon]
+    @current_temp = current_info[:current][:temp].to_i
+    @feels_like = current_info[:current][:feels_like].to_i
+    @humidity = current_info[:current][:humidity].to_i
+    @visibility = current_info[:visibility].nil? ? ('unvailable') : (current_info[:visibility] / 1609.34)
+    # @date = Time.at(current_info[:dt]).strftime("%m/%d/%Y")
+    # @time = current_info[:dt]
+    # @name = current_info[:name]
+    # @conditions = current_info[:weather].first[:description].titleize
+    # @icon = current_info[:weather].first[:icon]
+    # @current_temp = current_info[:main][:temp].to_i
+    # @feels_like = current_info[:main][:feels_like].to_i
+    # @high = current_info[:main][:temp_max].to_i
+    # @low = current_info[:main][:temp_min].to_i
+    # @humidity = current_info[:main][:humidity].to_i
   end
 end
--- app/poros/hourly_weather.rb
+++ app/poros/hourly_weather.rb
@@ -1,17 +1,11 @@
 class HourlyWeather
+  attr_reader :date, :time, :hour, :icon, :temp
   def initialize(hourly_info)
-    @info = hourly_info
+    @date = Time.at(hourly_info[:dt]).strftime("%d/%m/%Y")
+    @time = hourly_info[:dt]
+    @hour = Time.at(hourly_info[:dt]).strftime("%l %p")
+    @icon = hourly_info[:weather].first[:icon]
+    @temp = hourly_info[:temp].to_i
   end
 
-  def hour
-    Time.at(@info[:dt]).strftime("%l %p")
-  end
-
-  def icon
-    @info[:weather].first[:icon]
-  end
-
-  def temp
-    @info[:temp].to_i
-  end
 end
