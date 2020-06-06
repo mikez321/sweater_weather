0835543c622f4734958330229f77faa3515e25d8
Mike Hernandez <hernandezmichael321@gmail.com>
2020-06-06 16:16:03 -0600

    Add city name to forecast

--- app/services/forecast_search.rb
+++ app/services/forecast_search.rb
@@ -4,7 +4,7 @@ class ForecastSearch
     current_weather = WeatherService.current(city_name)
     hourly_weather = WeatherService.hourly(city_name)
     daily_weather = WeatherService.daily(city_name)
-    Forecast.new(current_weather, hourly_weather, daily_weather)
+    Forecast.new(current_weather, hourly_weather, daily_weather, city_name)
   end
 
 end
