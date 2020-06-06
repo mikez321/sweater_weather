a3d5533b203ca59c1d71d9401519686fd55dce60
Mike Hernandez <hernandezmichael321@gmail.com>
2020-06-06 16:59:08 -0600

    Break apart faraday in google to make it reusable

--- app/services/google_service.rb
+++ app/services/google_service.rb
@@ -1,10 +1,21 @@
 class GoogleService
   def self.get_coordinates(city_name)
-    response = Faraday.get('https://maps.googleapis.com/maps/api/geocode/json') do |f|
-      f.params[:address] = city_name
-      f.params[:key] = ENV['GOOGLE_KEY']
+    response = conn.get('geocode/json') do |conn|
+      conn.params[:address] = city_name
     end
     json = JSON.parse(response.body, symbolize_names: true)
     coords = json[:results].first[:geometry][:location]
+  end
+
+  def self.get_city_image(city_name)
+    
+  end
+
+  private
+
+  def self.conn
+    Faraday.new('https://maps.googleapis.com/maps/api') do |f|
+      f.params[:key] = ENV['GOOGLE_KEY']
+    end
   end
 end
