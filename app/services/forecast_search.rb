c128d1e09ae79e9d773d2a796971ab9e6c715467
Mike Hernandez <hernandezmichael321@gmail.com>
2020-06-06 16:46:17 -0600

    Remove blank space

--- app/services/forecast_search.rb
+++ app/services/forecast_search.rb
@@ -1,10 +1,8 @@
 class ForecastSearch
-
   def self.results(city_name)
     current_weather = WeatherService.current(city_name)
     hourly_weather = WeatherService.hourly(city_name)
     daily_weather = WeatherService.daily(city_name)
     Forecast.new(current_weather, hourly_weather, daily_weather, city_name)
   end
-
 end
