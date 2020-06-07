2aa93fcdc1a0ba49c5be2654547d7077eac86f18
Mike Hernandez <hernandezmichael321@gmail.com>
2020-06-07 11:40:31 -0600

    Fix all rubocop errors

--- app/poros/hourly_weather.rb
+++ app/poros/hourly_weather.rb
@@ -1,9 +1,9 @@
 class HourlyWeather
   attr_reader :date, :time, :hour, :icon, :temp
   def initialize(hourly_info)
-    @date = Time.at(hourly_info[:dt]).strftime('%d/%m/%Y')
+    @date = Time.at(hourly_info[:dt]).in_time_zone.strftime('%d/%m/%Y')
     @time = hourly_info[:dt]
-    @hour = Time.at(hourly_info[:dt]).strftime('%l %p')
+    @hour = Time.at(hourly_info[:dt]).in_time_zone.strftime('%H')
     @icon = hourly_info[:weather].first[:icon]
     @temp = hourly_info[:temp].to_i
   end
