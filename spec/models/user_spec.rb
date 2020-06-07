7cc6bfc97af34153688952e36afb48634ddb1b21
Mike Hernandez <hernandezmichael321@gmail.com>
2020-06-07 12:59:26 -0600

    Create test to validate pw digest

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'relationships' do
     it { should validate_presence_of :email }
+    it { should validate_presence_of :password_digest }
   end
 end
