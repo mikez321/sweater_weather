623b9a6f1260b161f412725b720cc2dd4d7ec0c6
Mike Hernandez <hernandezmichael321@gmail.com>
2020-06-07 16:58:29 -0600

    various rubocop fixes

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -13,6 +13,6 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-     params.permit(:email, :password, :password_confirmation)
+    params.permit(:email, :password, :password_confirmation)
   end
 end
--- app/poros/error_generator.rb
+++ app/poros/error_generator.rb
@@ -1,22 +1,21 @@
 class ErrorGenerator
   def initialize(user_errors)
     @status = '400'
-    @source = user_errors.details.keys.join(", ")
-    @title = user_errors.details.flat_map { |error, deets| deets }
+    @source = user_errors.details.keys.join(', ')
+    @title = user_errors.details.flat_map { |_error, deets| deets }
     @error = user_errors.full_messages.to_sentence
   end
 
   def error_hash
     {
-        "errors": [
-          {
-            "status": @status,
-            "source": @source,
-            "title":  @title,
-            "detail": @error
-          }
-        ]
-      }
+      "errors": [
+        {
+          "status": @status,
+          "source": @source,
+          "title": @title,
+          "detail": @error
+        }
+      ]
+    }
   end
-
 end
