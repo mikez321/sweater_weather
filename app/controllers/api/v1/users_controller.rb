0f000be1bf95d9bb4faa3129c864046eb7822885
Mike Hernandez <hernandezmichael321@gmail.com>
2020-06-07 15:36:40 -0600

    Create sad path for unsuccessful user creation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,24 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    user = User.create(user_params)
+    user = User.new(user_params)
+    if user.save
       user.create_api_key
       render json: UserSerializer.new(user).serializable_hash
+    else
+      error = user.errors.full_messages.to_sentence
+      type = user.errors.details.to_s
+      title = user.errors.details.keys.to_s
+      render json: {
+          "errors": [
+            {
+              "status": "400",
+              "source": type,
+              "title":  title,
+              "detail": error
+            }
+          ]
+        }
+    end
   end
 
   private
--- spec/requests/api/v1/new_users_spec.rb
+++ spec/requests/api/v1/new_users_spec.rb
@@ -27,4 +27,51 @@ describe 'new user registration' do
     expect(json[:data][:attributes]).to have_key(:api_key)
     expect(json[:data][:attributes][:api_key]).to_not be_nil
   end
+
+  it 'will not let a user use an already registered email' do
+    user1 = User.create!(email: 'mefirst@firstplace.com',
+                         password: 'winner',
+                         password_confirmation: 'winner')
+
+    params = {
+        "email": "mefirst@firstplace.com",
+        "password": "password",
+        "password_confirmation": "password"
+      }
+
+    post '/api/v1/users', params: params
+
+    expect(response).to be_successful
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(json).to have_key(:errors)
+    expect(json[:errors].first).to have_key(:status)
+    expect(json[:errors].first).to have_key(:source)
+    expect(json[:errors].first).to have_key(:title)
+    expect(json[:errors].first).to have_key(:detail)
+    expect(json[:errors].first[:status]).to eq('400')
+    expect(json[:errors].first[:detail]).to eq('Email has already been taken')
+  end
+
+  it 'will not create a user if pw and pw conf are mismatched' do
+    params = {
+        "email": "mefirst@firstplace.com",
+        "password": "password",
+        "password_confirmation": "drowssap"
+      }
+
+    post '/api/v1/users', params: params
+
+    expect(response).to be_successful
+    json = JSON.parse(response.body, symbolize_names: true)
+
+
+    expect(json).to have_key(:errors)
+    expect(json[:errors].first).to have_key(:status)
+    expect(json[:errors].first).to have_key(:source)
+    expect(json[:errors].first).to have_key(:title)
+    expect(json[:errors].first).to have_key(:detail)
+    expect(json[:errors].first[:status]).to eq('400')
+    expect(json[:errors].first[:detail]).to eq("Password confirmation doesn't match Password")
+  end
 end
