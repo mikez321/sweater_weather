8389e0b6a2ff1723515a18a86825e784c5ee98bb
Mike Hernandez <hernandezmichael321@gmail.com>
2020-06-08 15:02:23 -0600

    Finish functionality for successful login

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,5 +1,18 @@
 class Api::V1::SessionsController < ApplicationController
   def create
-    
+    user = User.find_by(email: login_params[:email])
+    if user.authenticate(login_params[:password])
+      session[:user_id] = user.id
+      render json: UserSerializer.new(user)
+    else
+      error_hash = ErrorGenerator.new(user.errors, 400).error_hash
+      render json: error_hash, status: :bad_request
+    end
+  end
+
+  private
+
+  def login_params
+    params.permit(:email, :password)
   end
 end
--- spec/requests/api/v1/user_login_spec.rb
+++ spec/requests/api/v1/user_login_spec.rb
@@ -1,12 +1,41 @@
 require 'rails_helper'
 
 describe 'user logs in' do
-  it 'can process a successful login' do
-    user = create(:user)
+  before(:each) do
 
-    post '/api/v1/sessions'
+    params = {
+        "email": "whatever@example.com",
+        "password": "password",
+        "password_confirmation": "password"
+      }
+
+    post '/api/v1/users', params: params
+  end
+
+  it 'can process a successful login' do
+
+    login_params = {
+      email: 'whatever@example.com',
+      password: 'password'
+    }
+
+    post '/api/v1/sessions', params: login_params
 
     expect(response).to be_successful
-    
+
+    expect(response.status).to eq(200)
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    user = User.find_by(email: login_params[:email])
+
+    expect(json).to have_key(:data)
+    expect(json[:data]).to have_key(:type)
+    expect(json[:data]).to have_key(:id)
+    expect(json[:data]).to have_key(:attributes)
+    expect(json[:data][:attributes]).to have_key(:email)
+    expect(json[:data][:attributes]).to have_key(:api_key)
+    expect(json[:data][:attributes][:email]).to eq(user.email)
+    expect(json[:data][:attributes][:api_key]).to eq(user.api_key)
   end
 end
