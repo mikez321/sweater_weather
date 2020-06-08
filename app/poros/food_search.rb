cd5ae04e8ae3bbfe52972c898d6f055bf65f995a
Mike Hernandez <hernandezmichael321@gmail.com>
2020-06-08 11:51:53 -0600

    Introduce class variables to cut down on clutter

--- app/poros/food_search.rb
+++ app/poros/food_search.rb
@@ -1,19 +1,19 @@
 class FoodSearch
+
   def self.result(food_params)
-    Foodie.new(restaurant(food_params), travel_time(food_params), end_location(food_params), short_forecast(food_params))
+    @@destination = food_params['end']
+    @@origin = food_params['start']
+
+    Foodie.new(restaurant(food_params), travel_time(food_params), @@destination, short_forecast(food_params))
   end
 
   def self.travel_time(food_params)
-    GoogleService.get_travel_time(food_params['start'], food_params['end'])
+    GoogleService.get_travel_time(@@origin, @@destination)
   end
 
   def self.short_forecast(food_params)
-    destination_weather = WeatherService.current(food_params['end'])
+    destination_weather = WeatherService.current(@@destination)
     ShortForecast.new(destination_weather)
-  end
-
-  def self.end_location(food_params)
-    food_params['end']
   end
 
   def self.restaurant(food_params)
