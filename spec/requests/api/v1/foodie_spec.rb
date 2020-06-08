8de3c7d2f64a735c580103c7d7164f5af68bc99a
Mike Hernandez <hernandezmichael321@gmail.com>
2020-06-08 09:30:33 -0600

    build basics of first test, getting successful results for directions and zomato in postman

---
+++ spec/requests/api/v1/foodie_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe 'foodie endpoint' do
+  it 'can return info about a place, food, and the weather' do
+
+    get '/api/v1/foodie?start=denver,co&end=pueblo,co&search=italian'
+
+    expect(response).to be_successful
+
+  end
+end
