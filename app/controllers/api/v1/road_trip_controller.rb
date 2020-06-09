3bd19d54ed6051ed70a09fa14417bc53dd3b307d
Mike Hernandez <hernandezmichael321@gmail.com>
2020-06-09 11:22:19 -0600

    Add controller action for checking api key before returning a road trip

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::RoadTripController < ApplicationController
+  before_action :valid_key?
+
   def create
     origin = road_trip_params[:origin]
     destination = road_trip_params[:destination]
@@ -10,5 +12,14 @@ class Api::V1::RoadTripController < ApplicationController
 
   def road_trip_params
     params.permit(:origin, :destination, :api_key)
+  end
+
+  def valid_key?
+    api_key = road_trip_params[:api_key]
+    if !api_key.nil? && !User.find_by(api_key: api_key).nil?
+    else
+      invalid_key_error = ErrorGenerator.invalid_key
+      render json: invalid_key_error, status: :unauthorized
+    end
   end
 end
