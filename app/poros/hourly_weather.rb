fb0da075968efefd897588da15285eeb4ab4af01
Mike Hernandez <hernandezmichael321@gmail.com>
2020-06-09 20:06:22 -0600

    Add description to hourly weather and beef up test

--- app/poros/hourly_weather.rb
+++ app/poros/hourly_weather.rb
@@ -1,10 +1,11 @@
 class HourlyWeather
-  attr_reader :date, :time, :hour, :icon, :temp
+  attr_reader :date, :time, :hour, :icon, :temp, :description
   def initialize(hourly_info)
     @date = Time.at(hourly_info[:dt]).in_time_zone.strftime('%d/%m/%Y')
     @time = hourly_info[:dt]
     @hour = Time.at(hourly_info[:dt]).in_time_zone.strftime('%H')
     @icon = hourly_info[:weather].first[:icon]
     @temp = hourly_info[:temp].to_i
+    @description = hourly_info[:weather].first[:main]
   end
 end
