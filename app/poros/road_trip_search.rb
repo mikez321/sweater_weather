257669d65e69fed14e0fe955224b71fb58a65ff7
Mike Hernandez <hernandezmichael321@gmail.com>
2020-06-09 20:25:43 -0600

    Add unix time to road trip api exposure and update test

--- app/poros/road_trip_search.rb
+++ app/poros/road_trip_search.rb
@@ -5,6 +5,7 @@ class RoadTripSearch
     RoadTrip.new(origin,
                  destination,
                  travel_time,
+                 arrival_time,
                  arrival_weather)
   end
 
@@ -16,5 +17,9 @@ class RoadTripSearch
     travel_time = GoogleService.travel_time_seconds(@origin, @destination)
     arrival_time = TimeCalculator.round_to_nearest_hour(travel_time)
     ForecastSearch.arrival_weather(@destination, arrival_time)
+  end
+
+  def self.arrival_time
+    Time.now + GoogleService.travel_time_seconds(@origin, @destination)
   end
 end
